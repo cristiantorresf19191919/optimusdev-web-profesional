2d1c1305777c1dc047b5c00907f869112b38fb01
cristian torres <cristian.torres19@hotmail.com>
2021-09-16 19:19:22 -0500

    Fix serverside rendering express

--- src/server/main.js
+++ src/server/main.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const app = new express();
+const app = express();
 
-app.use('/',express.static(path.resolve(__dirname,'../dist')))
+app.use('/', express.static(path.resolve(__dirname, '../../dist')))
 
-app.get('*',function (req, res) {
-    const pathToHtmlFile = path.resolve(__dirname,'../../dist/opttimusdev.html');
-    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile,'utf-8');
-    res.send(contentFromHtmlFile);
+app.get('*', function (req, res){
+    const pathToHtmlFile = path.resolve(__dirname, '../../dist/opttimusdev.html');
+    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+    res.send(contentFromHtmlFile)
 });
-const PORT = process.env.PORT || 9999;
+const PORT = process.env.PORT || 9090;
 app.listen(PORT, function () {
-    console.log("application is running on port "+PORT)
+    console.log('Application is running on port '+PORT)
 })
